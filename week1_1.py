2de69b53a4c7f2e05bf212af12197f374b298796
karthik8626 <kothakarthik8626@gmail.com>
2025-07-14 20:58:38 +0530

    Add files via upload

---
+++ week1_1.py
@@ -0,0 +1,15 @@
+#my first python program
+name="karthik"
+age=19
+print(name,age)
+#2
+a=19
+b=25
+print("before swapping",a,b)
+temp=a
+a=b
+b=temp
+print("after swpping",a,b)
+#3
+radius=3
+print("the radius of circle is:",3.14*radius**2)
